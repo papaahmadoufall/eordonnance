95aaff87bf3ab8f2019abc24476b991eee144526
papa ahmadou fall <pahmadoudou@gmail.com>
2022-06-10 17:56:35 +0000

    install laravel and creation model

---
+++ app/Http/Controllers/PharmacieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pharmacie;
+use Illuminate\Http\Request;
+
+class PharmacieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\pharmacie  $pharmacie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(pharmacie $pharmacie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\pharmacie  $pharmacie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(pharmacie $pharmacie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\pharmacie  $pharmacie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, pharmacie $pharmacie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\pharmacie  $pharmacie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(pharmacie $pharmacie)
+    {
+        //
+    }
+}
